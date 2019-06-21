a0873cb7155e3bb0bbea8778a87845efab01adc2
AnnaShalaginova <anya.shalaginova@gmail.com>
2019-06-21 16:26:05 -0400

    Commented out limit file

--- assets/scripts/templates/helpers/limit.js
+++ assets/scripts/templates/helpers/limit.js
@@ -1,15 +1,17 @@
-// HELPER EXAMPLE
-// This helper would be used in a .handlebars file
-// with the syntax {{limit title 20}}
-
-'use strict'
-
-const limit = (str, length) => {
-  if (str.length <= length) {
-    return str
-  } else {
-    return str.substring(0, length) + '...'
-  }
-}
-
-module.exports = limit
+// // HELPER EXAMPLE
+// // This helper would be used in a .handlebars file
+// // with the syntax {{limit title 20}}
+//
+// 'use strict'
+//
+// const limit = (str, length) => {
+//
+//   console.log(str)
+//   if (str.length <= length) {
+//     return str
+//   } else {
+//     return str.substring(0, length) + '...'
+//   }
+// }
+//
+// module.exports = limit
